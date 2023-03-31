e06091621ceda3107fc21658e90e94f2bd6acf0c
ign7 <quaresmaigor@yahoo.com>
2023-03-31 16:45:13 -0300

    -favicon

--- telainicial.php
+++ telainicial.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link  href="styleIndex.css" rel="stylesheet">
+    <link rel="icon" href="data:,">
     <title>Document</title>
 </head>
 <body>
